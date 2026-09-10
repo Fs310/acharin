233f7847d46926188b4717763689f6c5c8607087
Fs310 <116827676+Fs310@users.noreply.github.com>
2026-09-10 11:02:06 +0330

    Make issued invoice fields read-only in admin

--- Home/admin.py
+++ Home/admin.py
@@ -34,8 +34,22 @@ class ReviewAdmin(admin.ModelAdmin):
 class PartUsageInline(admin.TabularInline):
     model = PartUsage
     extra = 1
-    readonly_fields = ('unit_price',)
     fields = ('part', 'quantity', 'unit_price')
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj and obj.factor.is_issued:
+            return ('part', 'quantity', 'unit_price')
+        return ()
+
+    def has_delete_permission(self, request, obj=None):
+        if obj and obj.factor.is_issued:
+            return False
+        return super().has_delete_permission(request, obj)
+
+    def get_extra(self, request, obj=None):
+        if obj and obj.factor.is_issued:
+            return 0
+        return 1
 
 
 class FactorServiceAdmin(admin.ModelAdmin):
@@ -44,15 +58,33 @@ class FactorServiceAdmin(admin.ModelAdmin):
     search_fields = ('factor__client_fact__Cname', 'factor__client_fact__tel', 'service__title')
     inlines = [PartUsageInline]
 
+    def get_readonly_fields(self, request, obj=None):
+        if obj and obj.factor.is_issued:
+            return ('factor', 'service', 'price_service')
+        return ()
+
+    def has_delete_permission(self, request, obj=None):
+        if obj and obj.factor.is_issued:
+            return False
+        return super().has_delete_permission(request, obj)
+
 
 class FactorAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'client_fact', 'date_created', 'services_total',
-        'parts_total', 'total_without_tax', 'tax_amount', 'total_with_tax'
+        'id', 'client_fact', 'date_created', 'is_issued',
+        'services_total', 'parts_total', 'total_without_tax',
+        'tax_amount', 'total_with_tax'
     )
     search_fields = ('client_fact__Cname', 'client_fact__tel')
-    list_filter = ('tax_percent', 'date_created')
+    list_filter = ('is_issued', 'tax_percent', 'date_created')
     readonly_fields = ('services_total', 'parts_total', 'total_without_tax', 'tax_amount', 'total_with_tax')
+
+    def get_readonly_fields(self, request, obj=None):
+        fields = list(super().get_readonly_fields(request, obj))
+        if obj and obj.is_issued:
+            fields.append('is_issued')
+            fields.extend(['client_fact', 'tax_percent'])
+        return tuple(fields)
 
 
 class PartAdmin(admin.ModelAdmin):
