d446d945756696d52fc9f50af34fae693085ed4c
Fs310 <116827676+Fs310@users.noreply.github.com>
2026-09-10 10:37:41 +0330

    Protect account invoice and review views

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,6 @@ from django.utils.encoding import force_str, force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth.tokens import PasswordResetTokenGenerator, default_token_generator
-from django.db.models import Avg
 
 from Home.form import ReviewForm
 from .forms import *
@@ -35,13 +34,7 @@ def user_registration(request):
             email = form.cleaned_data['email']
             tel = form.cleaned_data['tel']
             password2 = form.cleaned_data['password2']
-            user = User.objects.create_user(
-                username=username,
-                first_name=firstname,
-                last_name=lastname,
-                email=email,
-                password=password2,
-            )
+            user = User.objects.create_user(username=username, first_name=firstname, last_name=lastname, email=email, password=password2)
             user.is_active = False
             user.save()
             profile, _ = Profile.objects.get_or_create(user=user)
@@ -65,13 +58,11 @@ def activate(request, uidb64, token):
         user = User.objects.get(id=uid)
     except (TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
-
     if user and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save(update_fields=['is_active'])
         messages.success(request, 'حساب کاربری شما فعال شد.')
         return redirect('accounts:login')
-
     messages.error(request, 'لینک فعال‌سازی معتبر نیست یا منقضی شده است.')
     return redirect('accounts:user_registration')
 
@@ -89,11 +80,9 @@ def login_user(request):
                     username = None
                 if username:
                     user = authenticate(request, username=username, password=data['password'])
-
             if user is not None:
                 login(request, user)
                 return redirect('home:homes')
-
             form.add_error(None, 'نام کاربری، ایمیل یا رمز عبور صحیح نیست.')
     else:
         form = UserLoginForm()
@@ -110,7 +99,6 @@ def profile(request):
     user = request.user
     profiles, _ = Profile.objects.get_or_create(user=user)
     list_order = Factor.objects.filter(client_fact__user=user).order_by('-date_created')
-
     if request.method == 'POST':
         form_profile = FormProfile(request.POST, request.FILES, instance=profiles)
         form_user = FormUser(request.POST, instance=user)
@@ -122,13 +110,7 @@ def profile(request):
     else:
         form_profile = FormProfile(instance=profiles)
         form_user = FormUser(instance=user)
-
-    return render(request, 'accounts/Profile.html', {
-        'profiles': profiles,
-        'form_user': form_user,
-        'form_profile': form_profile,
-        'list_order': list_order,
-    })
+    return render(request, 'accounts/Profile.html', {'profiles': profiles, 'form_user': form_user, 'form_profile': form_profile, 'list_order': list_order})
 
 
 @login_required(login_url='accounts:login')
@@ -154,20 +136,11 @@ def custom_password_reset_request(request):
                 user = User.objects.get(email=email)
             except User.DoesNotExist:
                 user = None
-
             if user:
                 uid = urlsafe_base64_encode(force_bytes(user.pk))
                 token = default_token_generator.make_token(user)
-                reset_link = request.build_absolute_uri(
-                    reverse('accounts:password_reset_confirm', args=[uid, token])
-                )
-                send_mail(
-                    'بازیابی رمز عبور آچارین',
-                    f'برای تغییر رمز عبور از لینک زیر استفاده کنید:\n{reset_link}',
-                    'fahime.s310@gmail.com',
-                    [email],
-                    fail_silently=False,
-                )
+                reset_link = request.build_absolute_uri(reverse('accounts:password_reset_confirm', args=[uid, token]))
+                send_mail('بازیابی رمز عبور آچارین', f'برای تغییر رمز عبور از لینک زیر استفاده کنید:\n{reset_link}', settings.DEFAULT_FROM_EMAIL, [email], fail_silently=False)
             return render(request, 'accounts/password_reset_done.html')
     else:
         form = CustomPasswordResetForm()
@@ -180,7 +153,6 @@ def password_reset_confirm(request, uidb64, token, password=None):
         user = User.objects.get(pk=uid)
     except (User.DoesNotExist, ValueError, TypeError, OverflowError):
         user = None
-
     if user is not None and default_token_generator.check_token(user, token):
         if request.method == 'POST':
             form = CustomSetPasswordForm(request.POST)
@@ -191,36 +163,33 @@ def password_reset_confirm(request, uidb64, token, password=None):
         else:
             form = CustomSetPasswordForm()
         return render(request, 'accounts/password_reset_confirm.html', {'form': form})
-
     return redirect('accounts:login')
 
 
+@login_required(login_url='accounts:login')
 def history(request):
     invoices = Factor.objects.filter(client_fact__user=request.user).order_by('-date_created')
     return render(request, 'accounts/history.html', {'invoices': invoices})
 
 
+@login_required(login_url='accounts:login')
 def detail_history(request, id):
     history_detail = get_object_or_404(Factor, id=id, client_fact__user=request.user)
     factor_services = history_detail.factor_services.select_related('service').prefetch_related('part_usages__part')
     reviews = set(history_detail.reviews.values_list('service_id', flat=True))
     for factor_service in factor_services:
         factor_service.has_review = factor_service.service_id in reviews
-    return render(request, 'accounts/detail_history.html', {
-        'history_detail': history_detail,
-        'factor_services': factor_services,
-    })
+    return render(request, 'accounts/detail_history.html', {'history_detail': history_detail, 'factor_services': factor_services})
 
 
+@login_required(login_url='accounts:login')
 def submit_review(request, factor_id, service_id):
     factor = get_object_or_404(Factor, id=factor_id, client_fact__user=request.user)
     factor_service = get_object_or_404(FactorService, factor=factor, service_id=service_id)
     service = factor_service.service
     existing_review = Review.objects.filter(factor=factor, service=service).first()
-
     if existing_review:
         return redirect('accounts:detail_history', factor.id)
-
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
@@ -232,10 +201,4 @@ def submit_review(request, factor_id, service_id):
             return redirect('accounts:detail_history', factor.id)
     else:
         form = ReviewForm()
-
-    return render(request, 'accounts/submit_review.html', {
-        'form': form,
-        'factor': factor,
-        'service': service,
-        'factor_service': factor_service,
-    })
+    return render(request, 'accounts/submit_review.html', {'form': form, 'factor': factor, 'service': service, 'factor_service': factor_service})
