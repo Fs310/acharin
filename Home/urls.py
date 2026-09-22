f55cb834f397f647ca904d6eb46dbcaa5f24b704
Fs310 <116827676+Fs310@users.noreply.github.com>
2026-09-22 22:39:46 +0330

    Remove article category route

--- Home/urls.py
+++ Home/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('service', views.service, name='services'),
     path('detail_service/<int:id>', views.detail_service, name='detail_service'),
     path('new', views.new, name='new'),
-    path('sub_new/<int:id>', views.new, name='sub_new'),
     path('detail_new/<str:slug>', views.detail_new, name='detail_new'),
     path('search', views.search_results, name='search_results'),
     path('repair', views.clients, name='clients'),
